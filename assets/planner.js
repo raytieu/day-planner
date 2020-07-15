b8e65c12eb40cb7c22a813edec5f40eb670a3962
Ray Tieu <ray.tieu@gmail.com>
2020-07-14 17:29:44 -0700

    created timeblocks using JS

--- assets/planner.js
+++ assets/planner.js
@@ -2,55 +2,59 @@ $(document).ready(function() {
 
     let dateTime = moment().format('MMMM Do YYYY, h:mm:ss a');
     $("#currentDay").text(dateTime);
-    
-    // function createRow() {
-    //     let row = $("<div>");
-    //     row.addClass("row");
-    //     row.add
-    //     $(".container").append(row);
 
-    //     console.log("hi");
+    let createRow = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+    let createHour = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+    let createTextarea = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+    let createButton = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+    let createSaveImg = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-    //     function createColumns() {
-    //         let column1 = $("<div>");
-    //         let column8 = $("<div>");
-    //         let column1A = $("<div>");
-    //         column1.addClass("col-1 time-block");
-    //         column8.addClass("col-8 description");
-    //         column1A.addClass("col-1 saveBtn");
-    //         $(".row").append(column1);
-    //         $(".row").append(column8);
-    //         $(".row").append(column1A);
-    //     }
+    for (let i = 9; i < 18; i++) {
 
-    //     createColumns();
+        createRow[i-9] = $("<row>");
+        createRow[i-9].attr("class", "row time-block");
+        $(".container").append(createRow[i-9]);
 
-    // }
+        createHour[i-9] = $("<div>");
+        createHour[i-9].attr("class", "col-1 hour");
+        if (i >= 9 && i <= 11) {
+            createHour[i-9].html(i + " am");
+        } else if (i === 12) {
+            createHour[i-9].html(i + " pm");
+        } else {
+            createHour[i-9].html((i-12) + " pm");
+        }
+        createRow[i-9].append(createHour[i-9]);
 
-    // createRow();
+        createTextarea[i-9] = $("<textarea>");
+        createTextarea[i-9].attr("class", "col-10 description");
+        createRow[i-9].append(createTextarea[i-9])
 
-    let textBox = $("#textarea"); 
+        createButton[i-9] = $("<button>");
+        createButton[i-9].attr("class", "col-1 saveBtn");
+        createRow[i-9].append(createButton[i-9]);
+
+        createSaveImg[i-9] = $("<i>");
+        createSaveImg[i-9].attr("class", "fa fa-save");
+        createButton[i-9].append(createSaveImg[i-9]);
+
+    }
+
+    let enterText = $(".description"); 
 
     $(".saveBtn").click(storeText);
 
     function storeText() {
         // textBox.html(textBox.val());
-        localStorage.setItem("textarea", textBox.val()); 
+        localStorage.setItem("textarea", enterText.val()); 
     }
 
-    function getText() {
-        let enterText = localStorage.getItem("textarea");
-        $("#textarea").html(enterText);    
+    function retrieveText() {
+        let getText = localStorage.getItem("textarea");
+        $(".description").html(getText);    
     }
 
-    $("#clear").click(clearStorage);
-
-    function clearStorage() {
-        localStorage.clear();
-        $("#textarea").empty();
-    };
-
-    getText();
+    retrieveText();
 
     console.log(localStorage);
     
